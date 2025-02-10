cac1efa6a149dff929d098ce3d899e65631b3c0d
MahaOfcl14 <itzmahaarun@gmail.com>
2025-02-10 16:56:45 +0530

    Frontend completed

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Welcome to our e-commerce clothing store! We offer a wide range of fashionable and affordable clothing for all occasions. From casual wear to formal attire, our collection is designed to meet the diverse needs of our customers. Shop with us and experience the best in quality, style, and customer service.
+            Our store features the latest trends and timeless classics, ensuring that you can find the perfect outfit for any event. We pride ourselves on providing high-quality products at competitive prices, and our dedicated team is always here to help you with any questions or concerns.</p>
+            <p>Discover the ultimate shopping experience at our online dress store. We specialize in offering a curated selection of dresses that cater to every style and preference. Whether you're looking for a chic cocktail dress, a stunning evening gown, or a casual sundress, we have something for everyone. Our commitment to quality and customer satisfaction ensures that you'll find the perfect dress for any occasion.</p>
+        </div>
+    </div>
+)
+}
+
+export default DescriptionBox
